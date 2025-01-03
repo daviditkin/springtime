5516d7101e0f3c26f501ab5b692f91489aa41621
David Itkin <ditkin@gmail.com>
2025-01-03 10:58:49 -0500

    initial SpringBootApplication and RestController with RequestMapping.  hello world.

---
+++ src/test/java/com/ditkin/springtime/SpringtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ditkin.springtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
